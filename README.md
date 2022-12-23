7c28d7c1c68394bed4b3d4c46f166b8a42979d84
Nil-Andreu <nilandreug@gmail.com>
2022-12-23 19:54:09 +0100

    [NEW] Cleaning notes

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Auto-Deep-Learning (Auto Deep Learning)
-With this package, you will be able to create, train and deploy neural networks automatically based on the input that you provide.
+[![Downloads](https://pepy.tech/badge/auto_deep_learning)](https://pepy.tech/project/auto_deep_learning) ![Version](https://img.shields.io/badge/version-0.1.1-blue) ![Python-Version](https://img.shields.io/badge/python-3.9-blue) ![issues](https://img.shields.io/github/issues/Nil-Andreu/auto_deep_learning) ![PyPI - Status](https://img.shields.io/pypi/status/auto_deep_learning) ![License](https://img.shields.io/github/license/Nil-Andreu/auto_deep_learning) 
+
+```auto_deep_learning```: with this package, you will be able to create, train and deploy neural networks automatically based on the input that you provide.
+
+## Installation
+Use the package manager [pip](https://pypi.org/project/pip/) to install auto_deep_learning.
 
 To install the package:
-```
+```bash
     pip install auto_deep_learning
+```
+
+If using an old version of the package,
+```bash
+    pip install --upgrade auto_deep_learning
 ```
--- setup.py
+++ setup.py
@@ -8,11 +8,24 @@ except (IOError, ImportError):
     long_description = open('README.md').read()
 
 
+classifiers = [
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+]
+
+
 setup(
     name='auto_deep_learning',
-    version='0.1',
+    version='0.1.1',
     description='Automation of the creation of the architecture of the neural network based on the input',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/Nil-Andreu/auto-deep-learning',
     author='Nil Andreu',
     author_email='nilandreug@email.com',
@@ -20,6 +33,7 @@ setup(
     keywords=[
         'deep learning',
         'machine learning',
+        'computer vision',
         'convolutional neural networks',
         'neural networks'
     ],
@@ -35,14 +49,5 @@ setup(
         'torchaudio==0.13.1',
         'pytest'
     ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',      # Define that your audience are developers
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-  ],
+    classifiers=classifiers,
 )
