ffbd19080df1c08e11783f13836c76bb76dde6dc
Nil-Andreu <nilandreug@gmail.com>
2022-12-23 23:28:02 +0100

    [NEW] Constants: batch size, test/valid size

--- auto_deep_learning/constants.py
+++ auto_deep_learning/constants.py
@@ -1,2 +1,10 @@
+# For image transformation
 MEAN_CONSTANTS = [0.485, 0.456, 0.406]
 STD_CONSTANTS = [0.229, 0.224, 0.225]
+
+# For batch processing
+BATCH_SIZE = 64
+
+# Train/Valid/Test Split
+TEST_SIZE = 0.05
+VALID_SIZE = 0.1
