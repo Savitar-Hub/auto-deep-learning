3285a3d974794eba58ce90c035f981d7d9c96e0e
Nil-Andreu <nilandreug@gmail.com>
2022-12-23 23:15:18 +0100

    [NEW] ImageTransformer: __init__

---
+++ auto_deep_learning/augmentation.py
@@ -0,0 +1,33 @@
+import torchvision.transforms as transforms
+
+
+class ImageTransformer(type):
+    def __init__(
+        self, 
+        rotation: int = 0,
+        resize: int = 224,
+        resized_crop: int = 224,
+        horizontal_flip: int = 0,
+        color_jitter_brightness: int = 0,
+        color_jitter_saturation: int = 0,
+        color_jitter_contrast: int = 0,
+        color_jitter_hue: int = 0,
+        normalize: bool = True,
+        resize_enabled: bool = False,
+        resized_crop_enabled: bool = True
+    ):
+
+        self.rotation = rotation
+        self.resize = resize
+        self.resized_crop = resized_crop
+        self.horizontal_flip = horizontal_flip
+        self.color_jitter_brightness = color_jitter_brightness
+        self.color_jitter_saturation = color_jitter_saturation
+        self.color_jitter_contrast = color_jitter_contrast
+        self.color_jitter_hue = color_jitter_hue
+        self.normalize = normalize
+        self.resize_enabled = resize_enabled
+        self.resized_crop_enabled = resized_crop_enabled
+
+
+    
