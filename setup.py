6e9a694ebc743d38d0e353fb5b828a2e212919ed
Nil-Andreu <nilandreug@gmail.com>
2022-12-23 19:07:46 +0100

    [NEW] Classifiers in setup

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ setup(
     url='https://github.com/Nil-Andreu/auto-nn',
     author='Nil Andreu',
     author_email='nilandreug@email.com',
+    keywords=[
+        'deep learning',
+        'machine learning',
+        'convolutional neural networks',
+        'neural networks'
+    ],
     license='MIT',
     packages=[
         'auto_nn',
@@ -18,5 +24,15 @@ setup(
         'torchvision==0.14.1',
         'torchaudio==0.13.1',
         'pytest'
-    ]
+    ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
 )
