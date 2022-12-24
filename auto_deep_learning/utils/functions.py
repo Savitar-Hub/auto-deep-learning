5c5b1fa0cf7241113f2731a464c0059d7888be46
Nil-Andreu <nilandreug@gmail.com>
2022-12-24 13:09:16 +0100

    [NEW] image_folder_convertion

--- auto_deep_learning/utils/functions.py
+++ auto_deep_learning/utils/functions.py
@@ -5,16 +5,7 @@ from typing import List, Dict
 
 
 def create_df_image_folder(
-    columns: List[str] = [
-        'image_path',
-        'class',
-        'split_type'
-    ],
-    dtypes: Dict[str, str] = {
-        'image_path': 'object',
-        'class': 'category',
-        'split_type': 'category'
-    }
+    columns, dtypes
 ) -> pd.DataFrame:
     """
     Create the dataframe that will be the base for the image folder dataset.
@@ -39,3 +30,69 @@ def create_df_image_folder(
 
     return df
 
+
+
+def image_folder_convertion(
+    parent_folder_path: str,
+    columns: List[str] = [
+        'image_path',
+        'class',
+        'split_type'
+    ],
+    dtypes: Dict[str, str] = {
+        'image_path': 'object',
+        'class': 'category',
+        'split_type': 'category'
+    }
+):
+
+    # Get the df we will use as the base one 
+    df = create_df_image_folder(
+        columns=columns,
+        dtypes=dtypes
+    )
+
+    # We get the folders, which we expect to be ['train', 'test']
+    split_folders: List[str] = os.listdir(parent_folder_path)
+
+    # TODO: Assess folder structure is correct
+    
+    for split_folder in split_folders:
+        # We get the path of .../train and .../test
+        split_path: str = parent_folder_path + '/' + split_folder
+
+        # List the values that we have inside of each folder, which would be the classes
+        children_classes: List[str] = os.listdir(split_path)
+
+        # For each class of each folder
+        for child_class in children_classes:
+            # Get the path of .../train/class
+            images_path: str = split_path + '/' + child_class
+
+            # List the images we have inside that folder
+            images_child_class_path: List[str] = os.listdir(images_path) 
+
+            # TODO: Asses they are files and of .jpg/png
+
+            class_list = [child_class] * len(images_child_class_path)
+            dtype_list = [split_folder] * len(images_child_class_path)
+
+            # TODO: This is hardtypping the columns, so not flexible (ImageFolder is not flexible neither)
+            df = df.append(
+                pd.DataFrame(
+                    data = {
+                        'class': class_list,
+                        'split_type': split_folder,
+                        'image_path': images_child_class_path,
+                })
+            )
+
+    
+    # TODO: Checks once the pandas dataframe created, classes in train = test
+
+    
+    pass
+
+
+if __name__ == '_main__':
+    image_folder_convertion('./parent_folder')
