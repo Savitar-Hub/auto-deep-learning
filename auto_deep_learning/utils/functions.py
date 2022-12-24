9a12e20ba562d23b76d5a3660b904452d1b51d45
Nil-Andreu <nilandreug@gmail.com>
2022-12-24 12:54:44 +0100

    create_df_image_folder

--- auto_deep_learning/utils/functions.py
+++ auto_deep_learning/utils/functions.py
@@ -1,1 +1,41 @@
-# From image folder structure to csv
+import os
+import logging
+import pandas as pd
+from typing import List, Dict
+
+
+def create_df_image_folder(
+    columns: List[str] = [
+        'image_path',
+        'class',
+        'split_type'
+    ],
+    dtypes: Dict[str, str] = {
+        'image_path': 'object',
+        'class': 'category',
+        'split_type': 'category'
+    }
+) -> pd.DataFrame:
+    """
+    Create the dataframe that will be the base for the image folder dataset.
+    
+    Parameters
+    ----------
+    columns : list
+        The columns that will be used for the dataframe
+    
+    dtypes : dict
+        Dictionary for the column name : column dtype for the dataframe
+    
+    Returns
+    -------
+    df : pd.DataFrame
+        The DataFrame that will be used for organizing the Image Folder information
+    """
+
+    df = pd.DataFrame(columns=columns)
+
+    df = df.astype(dtypes)
+
+    return df
+
