2c47047da2f8255cfd81f38ea4afa52e0ebfb72c
Nil-Andreu <nilandreug@gmail.com>
2022-12-25 11:24:30 +0100

    [NEW] ImageTransformer: type check

--- auto_deep_learning/utils/data_handler/img/augmentation.py
+++ auto_deep_learning/utils/data_handler/img/augmentation.py
@@ -1,8 +1,10 @@
 from typing import Optional
 
+from pydantic import validate_arguments
 import torchvision.transforms as transforms
 
 from auto_deep_learning.utils.constants import MEAN_CONSTANTS, STD_CONSTANTS
+from auto_deep_learning.utils.exceptions.data_handler.img import InvalidArgumentType
 
 
 class ImageTransformer(object):
@@ -20,7 +22,37 @@ class ImageTransformer(object):
         resize_enabled: Optional[bool] = False,
         resized_crop_enabled: Optional[bool] = True,
         color_jitter_enabled: Optional[bool] = False
-    ):
+    ) -> None:
+
+        # TODO: Refactor this assertions with new function --> function.py utilities
+        # Assertion of the type hints
+        for numerical_field in [
+            'rotation', 
+            'resize', 
+            'resized_crop', 
+            'color_jitter_brightness', 
+            'color_jitter_saturation', 
+            'color_jitter_contrast', 
+            'color_jitter_hue'
+        ]:
+            if not isinstance(locals()[numerical_field], int):
+                raise InvalidArgumentType(
+                    variable_name=numerical_field,
+                    variable_expected_type='int'
+                )
+        
+        for boolean_field in [
+            'horizontal_flip',
+            'normalize',
+            'resize_enabled',
+            'resized_crop_enabled',
+            'color_jitter_enabled'
+        ]:
+            if not isinstance(locals()[boolean_field], bool):
+                raise InvalidArgumentType(
+                    variable_name=boolean_field,
+                    variable_expected_type='bool'
+                )
 
         self.rotation = rotation
         self.resize = resize
@@ -50,14 +82,15 @@ class ImageTransformer(object):
         transformations = [transforms.RandomRotation(self.rotation)] if self.rotation else []
         transformations += [transforms.RandomResizedCrop(224)] if self.resize_enabled else []
         transformations += [transforms.RandomHorizontalFlip()] if self.horizontal_flip else []
-        transformations += [
-            transforms.ColorJitter(
-                saturation=self.color_jitter_saturation, 
-                contrast=self.color_jitter_contrast, 
-                hue=self.color_jitter_hue, 
-                brightness=self.color_jitter_brightness
-            )
-        ] if self.color_jitter_enabled else []
+        if self.color_jitter_enabled:
+            transformations += [
+                transforms.ColorJitter(
+                    saturation=self.color_jitter_saturation, 
+                    contrast=self.color_jitter_contrast, 
+                    hue=self.color_jitter_hue, 
+                    brightness=self.color_jitter_brightness
+                )
+            ]
         transformations += [transforms.ToTensor()]
         transformations += [
             transforms.Normalize(
