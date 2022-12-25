1e38e3adb370643e741b5eafb9f87be93b855bdc
Nil-Andreu <nilandreug@gmail.com>
2022-12-25 22:33:17 +0100

    [NEW] Exceptions module

---
+++ auto_deep_learning/utils/exceptions/data_handler/__init__.py
@@ -0,0 +1,2 @@
+from auto_deep_learning.utils.exceptions.data_handler.img import InvalidArgumentType
+from auto_deep_learning.utils.exceptions.data_handler.loader import InvalidFileExtension, InvalidSplitType, NoFolderData, ChildFileUnexpected
