8dcaddcb8c23a51ca35196009e59510191135d44
Nil-Andreu <nilandreug@gmail.com>
2022-12-25 23:17:37 +0100

    [NEW] Python 3.7 support

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ classifiers = [
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.7', 
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
