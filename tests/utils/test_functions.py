c6663a699bcfcef72e2eef7bfd432c3c2a799bee
Nil-Andreu <nilandreug@gmail.com>
2022-12-26 10:39:46 +0100

    [NEW] TestCreateDfImageFolder

--- tests/utils/test_functions.py
+++ tests/utils/test_functions.py
@@ -1,7 +1,15 @@
 import os
 import shutil
 import pytest
-from auto_deep_learning.utils.functions import image_folder_convertion
+from typing import List, Dict
+
+import pandas as pd
+
+from auto_deep_learning.utils.functions import (
+    create_df_image_folder, 
+    image_folder_convertion
+)
+
 
 BASE_DIR = './tests/utils/images'
 CHILD_DIR = ['train', 'test', 'valid']
@@ -16,7 +24,7 @@ def cleanup(request):
     def remove_test_dir():
         for child in CHILD_DIR:
             child_path = BASE_DIR + '/' + child
-            
+
             if os.path.exists(child_path) and \
                 os.path.isdir(child_path):
 
@@ -25,9 +33,38 @@ def cleanup(request):
     request.addfinalizer(remove_test_dir)
 
 
+class TestCreateDfImageFolder:
+    def test_creation_df(
+        self
+    ):
+
+        # TODO: Pass this as constants
+        columns: List[str] = ['image_path', 'class', 'split_type']
+
+        dtypes: Dict[str, str] = {
+            'image_path': 'object',
+            'class': 'category',
+            'split_type': 'category'
+        }
+
+        df = create_df_image_folder(
+            columns=columns,
+            dtypes=dtypes
+        )
+
+        assert df.empty
+        assert df.columns.values.tolist() == ['image_path', 'class', 'split_type']
+        assert isinstance(df, pd.DataFrame)
+
+
 class TestImageFolderConvertion:
     def test_creation_df(
         self
     ):
-        os.mkdir(BASE_DIR + '/train')
+        for child in CHILD_DIR:
+            child_path = BASE_DIR + '/' + child
+
+            os.mkdir(child_path)
+        
+
         
