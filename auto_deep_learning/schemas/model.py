1b1691e8ecb59caec3e9345a998593ba4f8ab4a4
Nil-Andreu <nilandreug@gmail.com>
2022-12-26 12:13:58 +0100

    [NEW] ModelPrediction

--- auto_deep_learning/schemas/model.py
+++ auto_deep_learning/schemas/model.py
@@ -1,6 +1,26 @@
+from typing import List
 from pydantic import BaseModel
 
 
-class ModelPredictionClass(BaseModel):
+class ModelPredictionClassUnit(BaseModel):
+    """
+    The prediction made for a class inside of a group of classes.
+    """
+
     class_name: str
     class_prediction: float
+
+
+class ModelPredictionUnit(BaseModel):
+    """
+    Multiple predictions made related to a class group.
+    """
+    prediction_unit: List[ModelPredictionClassUnit]
+
+
+class ModelPrediction(BaseModel):
+    """
+    Predictions of the model made for one or more class groups.
+    """
+    
+    prediction: List[ModelPredictionUnit]
