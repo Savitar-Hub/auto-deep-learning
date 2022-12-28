0a725c802f7579b5f05a686becae72d521413a51
Nil-Andreu <nilandreug@gmail.com>
2022-12-28 09:17:26 +0100

    [NEW] ConfigurationObject

--- auto_deep_learning/utils/config.py
+++ auto_deep_learning/utils/config.py
@@ -1,6 +1,61 @@
+from singleton import Singleton
+
 # For batch processing
 BATCH_SIZE = 64
 
 # Train/Valid/Test Split
 TEST_SIZE = 0.05
 VALID_SIZE = 0.1
+
+
+class ConfigurationObject(metaclass=Singleton):
+    def __init__(
+        self,
+        batch_size: int = 64,
+        valid_size: float = 64,
+        test_size: float = 0.05
+    ):
+
+        self._batch_size = batch_size
+        self._valid_size = valid_size
+        self._test_size = test_size
+    
+
+    @property
+    def batch_size(
+        self
+    ):
+        return self.batch_size
+    
+
+    @batch_size.setter
+    def batch_size(self, new_batch_size):
+        self._batch_size = new_batch_size
+
+        return self._batch_size
+    
+
+    @property
+    def valid_size(
+        self
+    ):
+        return self._valid_size
+    
+
+    @valid_size.setter
+    def valid_size(self, new_valid_size):
+        self._valid_size = new_valid_size
+        return self._valid_size
+
+    
+    @property
+    def test_size(
+        self
+    ):
+        return self._test_size
+    
+
+    @test_size.setter
+    def test_size(self, new_test_size):
+        self._test_size = new_test_size
+        return self._test_size
