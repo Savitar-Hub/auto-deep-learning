8e76fb9106546862e1f7e10ddd6428a6b45f8238
Nil-Andreu <nilandreug@gmail.com>
2022-12-29 09:04:36 +0100

    [NEW] Return a dict

--- auto_deep_learning/utils/data_handler/loader.py
+++ auto_deep_learning/utils/data_handler/loader.py
@@ -1,9 +1,11 @@
 import numpy as np
 from torch.utils.data import DataLoader
 from auto_deep_learning.utils import DataCreator
+from auto_deep_learning.utils.config import ConfigurationObject
 
 # Set a random seed in numpy
 np.random.seed(0)
+conf_obj = ConfigurationObject()
 
 class DatasetSampler():
     def __init__(
@@ -15,17 +17,10 @@ class DatasetSampler():
     
     def get_sampler(self):
         # TODO: For very small datasets, we might do not have valid
-        return [
-            DataLoader(
-                self.data_creator.get('train'), 
-                batch_size=32, 
-            ),
-            DataLoader(
-                self.data_creator.get('valid'), 
-                batch_size=32, 
-            ),
-            DataLoader(
-                self.data_creator.get('test'), 
-                batch_size=32, 
-            )
-        ]
+
+        return {
+            data_creator_key: DataCreator(
+                self.data_creator[data_creator_key],
+                batch_size = conf_obj.batch_size[data_creator_key]
+            ) for data_creator_key in self.data_creator.keys()
+        }
