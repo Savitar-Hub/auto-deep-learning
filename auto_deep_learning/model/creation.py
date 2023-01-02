50028179e0c0b1ff88987e241373837fdb7ee418
Nil-Andreu <nilandreug@gmail.com>
2023-01-02 12:01:25 +0100

    [NEW] Pre-commit hook

--- auto_deep_learning/model/creation.py
+++ auto_deep_learning/model/creation.py
@@ -1,29 +1,17 @@
-"""
-Things we take into consideration for creating the model:
-- Complexity of the task: how many images and how many classes we want to infer
-- Objective of the model: if a model if for production for a company, we might want to optimize tradeoff between throughtput and accuracy
-
-The training of the model:
-- Similarity with previous dataset: most of the models are trained with Imagenet and then we apply Transfer Learning. So depending on similarity, we make some warmup of the weights of the whole feature selection for some epochs or we only train the last layers.
-- Type of the model: each model supported has the HP for which it was trained
-"""
 from typing import Optional
 
-import torch
 import numpy as np
+import torch
 from torchsummary import summary
 
-from auto_deep_learning.enum import (
-    ModelObjective,
-    ModelName, 
-    OptimizerType
-)
-from auto_deep_learning.utils import DatasetSampler
-from auto_deep_learning.utils.config import ConfigurationObject
-from auto_deep_learning.utils.model import get_criterion, get_optimizer, default_weight_init
-from auto_deep_learning.utils.functions import to_cuda, count_model_parameters
+from auto_deep_learning.enum import ModelName
 from auto_deep_learning.model.definition import define_model
 from auto_deep_learning.model.inference import inference
+from auto_deep_learning.utils import DatasetSampler
+from auto_deep_learning.utils.config import ConfigurationObject
+from auto_deep_learning.utils.functions import count_model_parameters, to_cuda
+from auto_deep_learning.utils.model import (default_weight_init, get_criterion,
+                                            get_optimizer)
 
 conf_obj = ConfigurationObject()
 
@@ -33,48 +21,52 @@ class Model:
     def __init__(
         self,
         data: DatasetSampler,
-        description: Optional[str] = '',
-        objective: Optional[ModelObjective] = conf_obj.objective,
-        model_name: Optional[ModelName] = '',
-        model_version: Optional[str] = '',
+        category_type: Optional[str] = "",
+        objective: Optional[str] = conf_obj.objective,
+        model_name: Optional[ModelName] = "",
+        model_version: Optional[str] = "",
         input_shape: Optional[int] = conf_obj.image_size,
     ):
         """Instance of the Neural Network model.
 
         Args:
             data (Loader): the loader of the data that will be used
-            description (Optional[str], optional): short description of which task do you want to do. Defaults to None.
-            objective (Optional[ModelObjective], optional): definition of which is the objective. Defaults to throughput, as are the simpler and optimized for production environments.
-            model_name (Optional[ModelName], optional): definition of which is the model name (from HuggingFace). 
+            category_type (Optional[str], optional): short description of which task do you want to do. Defaults to None.
+            objective (Optional[ModelObjective], optional): definition of which is the objective.
+                Defaults to throughput, as are the simpler and optimized for production environments.
+            model_name (Optional[ModelName], optional): definition of which is the model name (from HuggingFace).
             model_version (Optional[str], optional): definition of which is the model version (from HuggingFace).
         """
-        
+
         self.data = data
         self.objective = objective
         self.model_name = model_name
-        self.description = description
+        self.category_type = category_type
         self.model_version = model_version
         self.input_shape = input_shape
 
         self.model = define_model(
             data=self.data,
-            description=self.description,
+            category_type=self.category_type,
             objective=self.objective,
             model_name=self.model_name,
             model_version=self.model_version,
-            input_shape=self.input_shape # TODO: Adapt to different input shapes
+            input_shape=self.input_shape,  # TODO: Adapt to different input shapes
         )
 
         self.criterion = get_criterion()
 
-
     @classmethod
     def fit(
-        self,
-        lr: Optional[int],  # TODO: Create function for default lr  -> 1e-4? Depends on self.model.recommended_lr, self.model.recommended_n_epochs
-        n_epochs: Optional[int] = conf_obj.n_epochs,  # TODO: Create function for default lr 
+        cls,
+        lr: Optional[
+            int
+        ],  # TODO: Create function for default lr  -> 1e-4? Depends on self.model.recommended_lr, self.model.recommended_n_epochs
+        n_epochs: Optional[
+            int
+        ] = conf_obj.n_epochs,  # TODO: Create function for default lr
         use_cuda: Optional[bool] = torch.cuda.is_available(),
-        save_path: Optional[str] = 'model.pt',
+        save_path: Optional[str] = "model.pt",
     ):
         """Train of the model
 
@@ -85,25 +77,25 @@ class Model:
             save_path (str, optional): the path to save the best model. Defaults to 'model.pt'.
 
         Returns:
-            _type_: _description_
-            
+            model: the model trained
+
         """
 
         # initialize tracker for minimum validation loss
-        valid_loss_min = np.Inf 
-        optimizer = get_optimizer(self.model, lr)
+        valid_loss_min = np.Inf
+        optimizer = get_optimizer(cls.model, lr)
 
         if use_cuda:
-            self.model.cuda()
+            cls.model.cuda()
 
-        for epoch in range(1, n_epochs+1):
+        for epoch in range(1, n_epochs + 1):
             # initialize variables to monitor training and validation loss
             train_loss, valid_loss = 0.0, 0.0
-            
-            # set the module to training mode
-            self.model.train()
 
-            for batch_idx, (data, target) in enumerate(self.data['train']):
+            # set the module to training mode
+            cls.model.train()
+
+            for batch_idx, (data, target) in enumerate(cls.data["train"]):
                 # move to GPU
                 if use_cuda:
                     # TODO: Multiple targets and data (for now data is only img)
@@ -111,8 +103,10 @@ class Model:
 
                 optimizer.zero_grad()
                 # Obtain the output from the model
-                output = self.model(data) # TODO: Multiple outputs, and each of them needs to be compared to the target
-                
+                output = cls.model(
+                    data
+                )  # TODO: Multiple outputs, and each of them needs to be compared to the target
+
                 # TODO: Check this works fine
                 # Obtain loss for each of the targets we have
                 for target_key in target.keys():
@@ -120,159 +114,139 @@ class Model:
                     target_expected = target[target_key]
 
                     if loss in locals():
-                        loss += self.criterion(
-                        target_output, 
-                        target_expected
-                    ) 
+                        loss += cls.criterion(target_output, target_expected)
 
                     else:
-                        loss = self.criterion(
-                            target_output, 
-                            target_expected
-                        )
+                        loss = cls.criterion(target_output, target_expected)
 
                 # Backward induction
                 loss.backward()
                 # Perform optimization step
-                optimizer.step()  
+                optimizer.step()
 
-                train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data.item() - train_loss))
+                train_loss = train_loss + (
+                    (1 / (batch_idx + 1)) * (loss.data.item() - train_loss)
+                )
                 del loss
 
             # set the model to evaluation mode
-            self.model.eval()
+            cls.model.eval()
 
-            if valid_loader := self.data.get('valid'):
+            if valid_loader := cls.data.get("valid"):
                 for batch_idx, (data, target) in enumerate(valid_loader):
                     # move to GPU
                     if use_cuda:
                         data, target = data.cuda(), target.cuda()
 
-                    output = self.model(data)
+                    output = cls.model(data)
+
                     # Obtain the loss
                     for target_key in target.keys():
-                        target_output = output[target_key]
-                        target_expected = target[target_key]
+                        target_output, target_expected = (
+                            output[target_key],
+                            target[target_key],
+                        )
 
                         if loss in locals():
-                            loss += self.criterion(
-                            target_output, 
-                            target_expected
-                        ) 
+                            loss += cls.criterion(target_output, target_expected)
 
                         else:
-                            loss = self.criterion(
-                                target_output, 
-                                target_expected
-                            )
+                            loss = cls.criterion(target_output, target_expected)
 
                     # Add this loss to the list (same as before but instead of train we use valid)
-                    valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data.item() - valid_loss))
+                    valid_loss = valid_loss + (
+                        (1 / (batch_idx + 1)) * (loss.data.item() - valid_loss)
+                    )
                     del loss
 
                 # TODO: Use logger instead of prints
-                # print training/validation statistics 
-                print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
-                    epoch, 
-                    train_loss,
-                    valid_loss
-                    ))
+                # print training/validation statistics
+                print(
+                    "Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}".format(
+                        epoch, train_loss, valid_loss
+                    )
+                )
 
                 if valid_loss < valid_loss_min:
                     # Print an alert
-                    print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model..'.format(
-                        valid_loss_min,
-                        valid_loss))
+                    print(
+                        "Validation loss decreased ({:.6f} --> {:.6f}).  Saving model..".format(
+                            valid_loss_min, valid_loss
+                        )
+                    )
 
-                    torch.save(self.model.state_dict(), save_path)
-                    
+                    torch.save(cls.model.state_dict(), save_path)
+
                     # Update the new minimum
                     valid_loss_min = valid_loss
-            
-        return self.model
-    
+
+        return cls.model
 
     @classmethod
-    def test(
-        self,
-        use_cuda: bool = torch.cuda.is_available()
-    ):
+    def test(cls, use_cuda: bool = torch.cuda.is_available()):
         # monitor test loss and accuracy
-        test_loss = 0.
-        correct = 0.
-        total = 0.
+        test_loss = 0.0
+        correct = 0.0
+        total = 0.0
 
         # set the module to evaluation mode
-        self.model.eval()
+        cls.model.eval()
 
         # Move cuda after eval so consumes less memory
         if use_cuda:
-            self.model.cuda()
+            cls.model.cuda()
 
-        if loader_test := self.data.get('test'):
+        if loader_test := cls.data.get("test"):
             for batch_idx, (data, target) in enumerate(loader_test):
                 # move to GPU
                 if use_cuda:
                     data, target = to_cuda(data), to_cuda(target)
 
                 # forward pass: compute predicted outputs by passing inputs to the model
-                output = self.model(data)
+                output = cls.model(data)
 
                 # calculate the loss
                 # TODO: Testing with multiple outputs & targets
-                loss = self.criterion(output, target)
+                loss = cls.criterion(output, target)
 
-                # update average test loss 
-                test_loss = test_loss + ((1 / (batch_idx + 1)) * (loss.data.item() - test_loss))
+                # update average test loss
+                test_loss = test_loss + (
+                    (1 / (batch_idx + 1)) * (loss.data.item() - test_loss)
+                )
 
                 # convert output probabilities to predicted class
                 pred = output.data.max(1, keepdim=True)[1]
 
                 # compare predictions to true label
-                correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+                correct += np.sum(
+                    np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy()
+                )
 
                 total += data.size(0)
-                    
-        print('Test Loss: {:.6f}\n'.format(test_loss))
 
-        print('\nTest Accuracy: %2d%% (%2d/%2d)' % (
-            100. * correct / total, 
-            correct, 
-            total)
+        print("Test Loss: {:.6f}\n".format(test_loss))
+
+        print(
+            "\nTest Accuracy: %2d%% (%2d/%2d)"
+            % (100.0 * correct / total, correct, total)
         )
 
-    
     @classmethod
-    def reset(
-        self
-    ):
+    def reset(cls):
 
-        self.model = default_weight_init(self.model)
-
+        cls.model = default_weight_init(cls.model)
 
     @classmethod
-    def predict(
-        self,
-        img_path: str = 'predict.img'
-    ):
-        output_inference = inference(
-            self.model,
-            img_path=img_path
-        )
+    def predict(cls, img_path: str = "predict.img"):
+        output_inference = inference(cls.model, img_path=img_path)
 
         return output_inference
-    
 
     @classmethod
-    def model_parameters(
-        self
-    ):
+    def model_parameters(cls):
         # TODO: Use log instead
-        print(summary(self.model, (self.input_shape)))
-
+        print(summary(cls.model, (cls.input_shape)))
 
     @property
-    def count_parameters(
-        self
-    ):
+    def count_parameters(self):
         return count_model_parameters(self.model)
--- auto_deep_learning/utils/model/optimizer.py
+++ auto_deep_learning/utils/model/optimizer.py
@@ -1,13 +1,13 @@
-from typing import Tuple, Any
+from typing import Any, Tuple
 
-import torch.nn as nn
 from torch import optim
+
 from auto_deep_learning.utils.config import ConfigurationObject
 
 conf_obj = ConfigurationObject()
 
 
-def weight_decay_params(params) -> Tuple[Any]:
+def weight_decay_params(params) -> Tuple[Any, Any]:
     """Get for which parameters we can have weight decay.
 
     Args:
@@ -30,17 +30,18 @@ def weight_decay_params(params) -> Tuple[Any]:
 def get_optimizer(
     model,
     lr: float = 1e-4,
-    filter_by_requires_grad = False,
-    betas: Tuple[float] = (.9, .99),
+    filter_by_requires_grad=False,
+    betas: Tuple[float, float] = (0.9, 0.99),
     eps: float = 1e-8,
-    wd: float = 1e-2
+    wd: float = 1e-2,
 ):
     """Get optimizer for the model parameters
 
     Args:
         model (Model): the model for which we want to apply an optimizer
         lr (float, optional): learning rate of the optimizer. Defaults to 1e-4.
-        filter_by_requires_grad (bool, optional): if we want to filter by the params that have gradient desc activated. Defaults to False.
+        filter_by_requires_grad (bool, optional): if we want to filter by the params that have gradient desc activated.
+            Defaults to False.
         betas (Tuple[float], optional): the betas for the Adam optimizer. Defaults to (.9, .99).
         eps (float, optional): the epsilon for the Adam optimizer. Defaults to 1e-8.
         wd (float, optional): weight decay of the learning rate. Defaults to 1e-2.
@@ -55,27 +56,19 @@ def get_optimizer(
     if filter_by_requires_grad:
         params = list(filter(lambda p: p.required_grad, params))
 
-    if conf_obj.optimizer == 'adam':
+    if conf_obj.optimizer == "adam":
         if wd == 0:
-            return optim.Adam(
-                params, 
-                lr = lr,
-                betas = betas,
-                eps = eps
-            )
+            return optim.Adam(params, lr=lr, betas=betas, eps=eps)
 
         # Get which parameters can have weight decay
         wd_params, not_wd_params = weight_decay_params(params)
 
         params = [
-            {'params': wd_params},
-            {'params': not_wd_params, 'weight_decay': 0},   # For the ones that do not have wd
+            {"params": wd_params},
+            {
+                "params": not_wd_params,
+                "weight_decay": 0,
+            },  # For the ones that do not have wd
         ]
 
-        return optim.AdamW(
-            params, 
-            lr = lr,
-            weight_decay = wd,
-            betas = betas,
-            eps = eps
-        )
+        return optim.AdamW(params, lr=lr, weight_decay=wd, betas=betas, eps=eps)
